cb72696a262a329934b242d1b8f82b7d0538c810
Daniel Araujo <daniel.araujo.inbox@gmail.com>
2026-08-07 02:33:29 +0000

    Fix staging and production environment configurations

--- environments/staging/variables.tf
+++ environments/staging/variables.tf
@@ -2,3 +2,8 @@ variable "admin_cidr" {
   description = "Public IP address allowed to SSH to the bastion host"
   type        = string
 }
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
