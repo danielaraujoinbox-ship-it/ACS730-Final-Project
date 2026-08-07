b4830583d5a3ddf8073809840154fdda08789f0d
Daniel Araujo <daniel.araujo.inbox@gmail.com>
2026-08-07 02:25:45 +0000

    Fix separate Terraform state for staging and production

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "acs730-dev-daniel009"
-    key     = "terraform-state/dev/terraform.tfstate"
+    key = "prod/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
